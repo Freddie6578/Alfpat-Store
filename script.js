6feb9096393ad7e954122ffa17e683ddf4595a10
Freddie6578 <newm6578@gmail.com>
2024-10-24 19:59:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const add = document.querySelector('.add')
 const minus = document.querySelector('.minus')
 const input = document.querySelector('.quantity-field input')
+const mobileCategory = document.querySelector('.mobile-product-category')
+const dropdown = document.querySelector('.mobile-product-category .container')
 
-add.addEventListener('click',()=>{
-    input.value++
+if (add) {    
+    add.addEventListener('click',()=>{
+        input.value++
+        
+    })
+}
+
+if (minus) {
+    minus.addEventListener('click',()=>{
+        input.value--
+    })
     
-})
+}
 
-minus.addEventListener('click',()=>{
-    input.value--
-})
+
+if (mobileCategory && dropdown) {
+    mobileCategory.addEventListener('click', (event) => {
+    dropdown.classList.toggle('active');
+    const categoryDisplayText = document.querySelector('.category-display-text')
+    const categoryItems = document.querySelectorAll('.mobile-product-category .container span')
+        console.log(categoryItems);
+        
+    
+    categoryItems.forEach((item) => {
+        item.addEventListener('click', () =>{
+            categoryDisplayText.innerText = item.innerText
+            console.log('clicked');
+            
+        })
+    });
+});
+}
+
+
